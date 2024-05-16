b73fe07a496ec7ab349a22c2f0788e3715882637
Ala Sharkia <[Ala.sharkia@gmail.com]>
2024-05-16 13:54:56 +0300

    aaa

--- script.js
+++ script.js
@@ -1,9 +1,116 @@
 const arr = [];
-
 function apiChoose() {
     const chooseElement = document.querySelector("#choose");
     const chooseValue = chooseElement.value;
     const results = document.querySelector(".result");
+
+    if (chooseValue === "films") {
+        filmsFetch(chooseValue);
+    }
+    else if (chooseValue === "people") {
+        peopleFetch(chooseValue);
+    }
+    else if (chooseValue === "planets") {
+        planetsFetch(chooseValue);
+    } else if (chooseValue === "species") {
+        speciesFetch(chooseValue);
+    } else if (chooseValue === "starships") {
+        starshipsFetch(chooseValue);
+
+    }
+
+    function filmsFetch() {
+        startLoader()
+        document.querySelector('tbody').style.display = 'none';
+        document.querySelector('tbody').innerHTML = '';
+        document.querySelector('th').innerHTML = "Title";
+        document.querySelector('th:nth-child(2)').innerHTML = "Director";
+        document.querySelector('th:nth-child(3)').innerHTML = "Release_date";
+        document.querySelector('th:nth-child(4)').innerHTML = "Episode_id";
+        document.querySelector('th:nth-child(5)').innerHTML = "Producer";
+        document.querySelector('th:nth-child(6)').innerHTML = "Characters";
+
+        let charactersCount = 0; // Track number of characters fetched
+        let totalCharacters; // Total number of characters to be fetched
+
+        fetch(`https://swapi.dev/api/${chooseValue}/`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("fetch unsuccessful");
+                } else {
+                    return res.json();
+                }
+            })
+            .then(data => {
+                const arr = data.results;
+                totalCharacters = arr.reduce((total, arr) => total + arr.characters.length, 0);
+                console.log(arr);
+                arr.forEach(elem => {
+                    const tr = document.createElement("tr");
+                    tr.innerHTML = `
+                    <td>${elem.title}</td>
+                    <td>${elem.director}</td>
+                    <td>${elem.release_date}</td>
+                    <td>${elem.episode_id}</td>
+                    <td>${elem.producer}</td>
+                    <td class="characters"></td>
+                `;
+                    document.querySelector('tbody').appendChild(tr);
+
+                    const charactersTd = tr.querySelector('.characters');
+
+                    elem.characters.forEach(characterUrl => {
+                        fetch(characterUrl)
+                            .then(characterResponse => {
+                                if (!characterResponse.ok) {
+                                    throw new Error("Character fetch unsuccessful");
+                                }
+                                return characterResponse.json();
+                            })
+                            .then(characterData => {
+                                const characterId = extractId(characterData.url);
+                                const characterImg = document.createElement('img');
+                                characterImg.src = `https://starwars-visualguide.com/assets/img/characters/${characterId}.jpg`;
+                                characterImg.alt = characterData.name;
+                                charactersTd.appendChild(characterImg);
+
+                                // Increment charactersCount when a character is fetched
+                                charactersCount++;
+
+                                // Check if all characters are fetched
+                                if (charactersCount === totalCharacters) {
+                                    // Display tbody when all pictures are loaded
+                                    document.querySelector('tbody').style.display = 'table-row-group';
+                                    stopLoader();
+
+
+                                }
+                            })
+                            .catch(err => {
+                                console.error('Error fetching character data:', err);
+                            });
+                    });
+                })
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+}
+
+function peopleFetch(chooseValue) {
+    startLoader()
+    document.querySelector('tbody').style.display = 'none';
+    document.querySelector('tbody').innerHTML = '';
+    document.querySelector('th').innerHTML = "name";
+    document.querySelector('th:nth-child(2)').innerHTML = "birth_year";
+    document.querySelector('th:nth-child(3)').innerHTML = "gender";
+    document.querySelector('th:nth-child(4)').innerHTML = "height";
+    document.querySelector('th:nth-child(5)').innerHTML = "skin_color";
+    document.querySelector('th:nth-child(6)').innerHTML = "films";
+
+    let filmsCount = 0; // Track number of characters fetched
+    let totalFilms; // Total number of characters to be fetched
 
     fetch(`https://swapi.dev/api/${chooseValue}/`)
         .then(res => {
@@ -15,34 +122,48 @@ function apiChoose() {
         })
         .then(data => {
             const arr = data.results;
+            totalFilms = arr.reduce((total, arr) => total + arr.films.length, 0);
+            console.log(arr);
             arr.forEach(elem => {
                 const tr = document.createElement("tr");
                 tr.innerHTML = `
-                    <td>${elem.title}</td>
-                    <td>${elem.director}</td>
-                    <td>${elem.release_date}</td>
-                    <td>${elem.episode_id}</td>
-                    <td>${elem.producer}</td>
-                    <td class="characters"></td>
+                    <td>${elem.name}</td>
+                    <td>${elem.birth_year}</td>
+                    <td>${elem.gender}</td>
+                    <td>${elem.height}</td>
+                    <td>${elem.skin_color}</td>
+                    <td class="films"></td>
+
                 `;
                 document.querySelector('tbody').appendChild(tr);
 
-                const charactersTd = tr.querySelector('.characters');
+                const filmsTd = tr.querySelector('.films');
 
-                elem.characters.forEach(characterUrl => {
-                    fetch(characterUrl)
-                        .then(characterResponse => {
-                            if (!characterResponse.ok) {
-                                throw new Error("Character fetch unsuccessful");
+                elem.films.forEach(filmsUrl => {
+                    fetch(filmsUrl)
+                        .then(filmsResponse => {
+                            if (!filmsResponse.ok) {
+                                throw new Error("films fetch unsuccessful");
                             }
-                            return characterResponse.json();
+                            return filmsResponse.json();
                         })
-                        .then(characterData => {
-                            const characterId = extractId(characterData.url);
-                            const characterImg = document.createElement('img');
-                            characterImg.src = `https://starwars-visualguide.com/assets/img/characters/${characterId}.jpg`;
-                            characterImg.alt = characterData.name;
-                            charactersTd.appendChild(characterImg);
+                        .then(filmsData => {
+                            const filmsId = extractId(filmsData.url);
+                            const filmsImg = document.createElement('img');
+                            filmsImg.src = `https://starwars-visualguide.com/assets/img/films/${filmsId}.jpg`;
+                            filmsImg.alt = filmsData.name;
+                            filmsTd.appendChild(filmsImg);
+                            // Increment charactersCount when a character is fetched
+                            filmsCount++;
+
+                            // Check if all characters are fetched
+                            if (filmsCount === totalFilms) {
+                                // Display tbody when all pictures are loaded
+                                document.querySelector('tbody').style.display = 'table-row-group';
+                                stopLoader();
+
+
+                            }
                         })
                         .catch(err => {
                             console.error('Error fetching character data:', err);
@@ -55,7 +176,14 @@ function apiChoose() {
         });
 }
 
-// Simplified extractId function
+
 function extractId(url) {
     return url.split('/').slice(-2, -1)[0];
+}
+
+function startLoader() {
+    document.querySelector('.loader').style.display = 'block';
+}
+function stopLoader() {
+    document.querySelector('.loader').style.display = 'none';
 }
